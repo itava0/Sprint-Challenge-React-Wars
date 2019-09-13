df5da514eec8f8c258d127f65831e5b8fff9d633
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-13 12:08:31 -0400

    Created another component named StarWarsCards

--- starwars/src/components/StarWarsCards.js
+++ starwars/src/components/StarWarsCards.js
@@ -1,3 +1,35 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Cards = styled.div `
+
+`
+const Title = styled.h2 `
+`
+const Name = styled.h3 `
+`
+const Gender = styled.p `
+` 
+const Birth = styled.p `
+`
+const Height = styled.p `
+`
+const EyeColor = styled.p `
+`
+const StarWarsCards = props =>{
+ return (
+  <Cards key={props.name}>
+  <Title>StarWars</Title> 
+  <Name>{props.title}</Name> 
+  <Gender>{props.gender}</Gender> 
+  <Birth>{props.birthDate}</Birth> 
+  <Height>{props.height}</Height> 
+  <EyeColor>{props.eyeColor}</EyeColor> 
+  </Cards>
+ );
+}
+
+export default StarWarsCards;
 
 
 
