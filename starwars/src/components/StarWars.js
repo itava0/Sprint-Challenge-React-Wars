3d3dd1da9e52ea520ebafa080e4274250e12d4b2
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-13 11:37:15 -0400

    Created a component named StarWars

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import StarWarsCards from "./StarWarsCards";
+import axios from "axios";
+
+function StartWars() {
+  const [star, setstar] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => {
+        const people = res.data.results;
+        console.log(res.data.results);
+        setstar(people);
+      })
+      .catch(err => {
+        console.log("The Data was not returned", err);
+      });
+  }, []);
+  return (
+    <div>
+      {star.map(person => {
+        return (
+          <StarWarsCards 
+          key={person.name} 
+          title={person.name}
+          birthDate={person.birth_year}
+          gender={person.gender}
+          height={person.height}
+          eyeColor={person.eye_color}/>
+        )
+      })}
+    </div>
+  );
+}
+
+export default StartWars;
